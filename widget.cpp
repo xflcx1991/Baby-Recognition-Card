0ac5e2fb22307f599a42ca77de7d0766b5de5ce3
chengxiangliu <xffish@126.com>
2019-12-04 22:17:52 +0800

    first commit & add files

---
+++ widget.cpp
@@ -0,0 +1,195 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QBitmap>
+#include <QBrush>
+#include <QDebug>
+#include <QImage>
+#include <QPainter>
+#include <QPalette>
+#include <QPixmap>
+#include <QRect>
+#include <QSignalMapper>
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+    , m_currentDrawImageIndx(0)
+{
+    ui->setupUi(this);
+    //    this->setPalette(QPalette(Qt::transparent));
+    //    this->setAutoFillBackground(true);
+
+    //设置左右按钮
+    QPixmap pixmapLeft(":/images/left");
+    ui->btnLeft->setMask(QBitmap(pixmapLeft.mask()));
+    ui->btnLeft->resize(pixmapLeft.size());
+
+    QPixmap pixmapRight(":/images/right");
+    ui->btnRight->setMask(QBitmap(pixmapRight.mask()));
+    ui->btnRight->resize(pixmapRight.size());
+
+    ui->frame->installEventFilter(this);
+
+    //    painter = nullptr;
+
+    //初始化图片名字列表
+    initImageFileNameList();
+    //初始化信号槽
+    //    connect(ui->btnLeft, &QPushButton::clicked, this, &Widget::switchButtonClicked);
+    //    connect(ui->btnRight, &QPushButton::clicked, this, &Widget::switchButtonClicked);
+    connect(ui->btnLeft, &QPushButton::clicked, [=]() { switchButtonClicked(-1); });
+    connect(ui->btnRight, &QPushButton::clicked, [=]() { switchButtonClicked(1); });
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+/**
+ * @brief 初始化图片名字列表
+ */
+void Widget::initImageFileNameList()
+{
+    //有15张图片
+    for (int i = 0; i < IMAGENUM; ++i) {
+        QString imageName = IMAGENAMEPATTERN.arg(i);
+
+        m_imageFileNameList.append(imageName);
+    }
+}
+
+void Widget::switchButtonClicked(int i)
+{
+    m_currentDrawImageIndx += i;
+    if (m_currentDrawImageIndx < 0) {
+        m_currentDrawImageIndx = IMAGENUM - 1;
+    }
+    if (m_currentDrawImageIndx >= IMAGENUM) {
+        m_currentDrawImageIndx = 0;
+    }
+    //    paintOnWidget(ui->frame);
+    ui->frame->update();
+    //qDebug() << m_currentDrawImageIndx;
+}
+/*
+void Widget::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event)
+    //创建QPainter对象
+    QPainter painter(ui->frame);
+    //    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setRenderHint(QPainter::TextAntialiasing);
+    //    int W = this->width();
+    //    int H = this->height();
+    int W = ui->frame->width();
+    int H = ui->frame->height();
+    const int width = 1500;
+    const int height = 1200;
+    int availableW = 0;
+    int availabelH = 0;
+    int side = qMin(W, H);
+    if (W > H) {
+        double xi = static_cast<double>(side) / static_cast<double>(height);
+        availabelH = side;
+        availableW = static_cast<int>(width * xi);
+    } else {
+        double xi = static_cast<double>(side) / static_cast<double>(width);
+        availabelH = static_cast<int>(height * xi);
+        availableW = side;
+    }
+    qDebug() << W << H;
+    qDebug() << availableW << availabelH;
+    qDebug() << "*********************************";
+    painter.setViewport((W - availableW) / 2, (H - availabelH) / 2, availableW, availabelH);
+    painter.setWindow(-(width / 2), -(height) / 2, width, height);
+    //    painter.setWindow(0, 0, width, height);
+
+    QRect rect(-(width / 2), -(height) / 2, width, height);
+    //    QRect rect(0, 0, width, height);
+
+    //设置画笔
+    //    QPen pen;
+    //    pen.setWidth(1);
+    //    pen.setColor(Qt::red);
+    //    pen.setStyle(Qt::SolidLine);
+    //    pen.setCapStyle(Qt::FlatCap);
+    //    pen.setJoinStyle(Qt::BevelJoin);
+    //    painter.setPen(pen);
+    //设置画刷
+    //    QPixmap texturePixmap(":images/screen");
+    //    QBrush brush;
+    //    brush.setColor(Qt::yellow);
+    //    brush.setStyle(Qt::TexturePattern);
+    //    brush.setTexture(texturePixmap);
+    //    painter.setBrush(brush);
+    //绘图
+    //    painter.drawRect(rect);
+    //    painter.drawRect(rect);
+    painter.drawImage(rect, QImage(":images/fruit_12.jpg"));
+}
+*/
+bool Widget::eventFilter(QObject *watched, QEvent *e)
+{
+    if (watched == ui->frame) {
+        if (e->type() == QEvent::Paint) {
+            paintOnWidget(ui->frame);
+
+            return true;
+        }
+    }
+
+    return QWidget::eventFilter(watched, e);
+}
+
+void Widget::paintOnWidget(QWidget *w)
+{
+    qDebug() << "paintOnWidget";
+    //创建QPainter对象
+    QPainter painter(w);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setRenderHint(QPainter::TextAntialiasing);
+    /*
+    if (painter == nullptr) {
+        painter = new QPainter(w);
+        painter->setRenderHint(QPainter::Antialiasing);
+        painter->setRenderHint(QPainter::TextAntialiasing);
+    }
+*/
+    qDebug() << w->metaObject()->className();
+    qDebug() << w->styleSheet();
+    qDebug() << "================";
+    int W = w->width();
+    int H = w->height();
+    const int width = 1500;
+    const int height = 1200;
+    int availableW = 0;
+    int availabelH = 0;
+    int side = qMin(W, H);
+    if (W > H) {
+        double xi = static_cast<double>(side) / static_cast<double>(height);
+        availabelH = side;
+        availableW = static_cast<int>(width * xi);
+    } else {
+        double xi = static_cast<double>(side) / static_cast<double>(width);
+        availabelH = static_cast<int>(height * xi);
+        availableW = side;
+    }
+    qDebug() << W << H;
+    qDebug() << availableW << availabelH;
+    qDebug() << "*********************************";
+    painter.setViewport((W - availableW) / 2, (H - availabelH) / 2, availableW, availabelH);
+    painter.setWindow(-(width / 2), -(height) / 2, width, height);
+    //    painter.setWindow(0, 0, width, height);
+
+    QRect rect(-(width / 2), -(height) / 2, width, height);
+    qDebug() << m_imageFileNameList;
+    qDebug() << m_currentDrawImageIndx;
+    qDebug() << m_imageFileNameList.at(m_currentDrawImageIndx);
+    painter.drawImage(rect, QImage(m_imageFileNameList.at(m_currentDrawImageIndx)));
+    painter.end();
+    //要或不要都能运行，内存占用增长也均一样，都保持在同一个数值后不变了
+    //    delete painter;
+    //    painter = nullptr;
+}
---
+++ widget.h
@@ -0,0 +1,42 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+//class QPainter;
+QT_BEGIN_NAMESPACE namespace Ui
+{
+    class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget() override;
+
+protected:
+    //    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+    virtual bool eventFilter(QObject *watched, QEvent *e) Q_DECL_OVERRIDE;
+    void paintOnWidget(QWidget *w);
+
+private:
+    Ui::Widget *ui;
+    //图片列表
+    QList<QString> m_imageFileNameList;
+    //当前显示图片index
+    int m_currentDrawImageIndx;
+    // 切换图片;
+    QImage m_currentImage;
+    QImage m_nextImage;
+    const int IMAGENUM = 15;
+    const QString IMAGENAMEPATTERN = ":images/fruit_%1.jpg";
+    //    QPainter *painter;
+    void initImageFileNameList();
+    void paintByPainter(QPainter painter);
+private slots:
+    void switchButtonClicked(int);
+};
+#endif // WIDGET_H
